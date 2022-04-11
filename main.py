fe8cc2ddc4b15ad8127cb2d7db44486f6c6d4f4a
Trevor <notmetro2008@gmail.com>
2022-04-11 19:45:50 +0100

    Last commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import *
 import turtle
+import random
+import time
 
 #creating screen
 screen = turtle.Screen()
@@ -33,9 +35,43 @@ Mercedes.pensize(10)
 Mercedes.color("white")
 Mercedes.write("Finish Line!", font =("Verdana", 10 , "bold"))
 
+#moving racer1 back
 Mercedes.penup()
 Mercedes.goto(-300,0)
 Mercedes.color("blue")
+Mercedes.right(90)
+
+#showing choice to bet to the player
+print("Welcome to F1!")
+bet = textinput("","""Welcome to F1, Which racer do you think will win?
+type 1 for blue or type 2 for red            
+""")
+
+#creating 2 dices with random numbers from 1-6
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+
+#if a racer cresses finish line it shows that they have won
+for i in range(30):
+    if Mercedes.position() > (300,250):
+        print("Mercedes is the winner!")
+        if int(bet) == 1:
+            print("your bet was right")
+        else:
+            print("Sorry, you were wrong")
+        break
+    elif redBull.position() > (300,-250):
+        print("RedBull is the winner!")
+        if int(bet) == 2:
+            print("your bet was right")
+        else:
+            print("Sorry, you were wrong")
+        break
+    else:
+        Mercedes.forward(20*dice1)
+        time.sleep(0.5)
+        redBull.forward(20*dice2)
+        time.sleep(0.5)
 
 
 

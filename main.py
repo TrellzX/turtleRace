1b14824c0fbf45b4a38a63010384f50eba6aaa24
Trevor <notmetro2008@gmail.com>
2022-04-11 19:17:34 +0100

    Commiting finishline

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 from turtle import *
 import turtle
+
 #creating screen
-screen = turtle.screen()
-screen.bgcolor("grey")
+screen = turtle.Screen()
+screen.bgcolor("black")
 
-
-#
+#creating racer 1
 Mercedes = Turtle()
 Mercedes.color("Blue")
 Mercedes.shape("square")
 
+#creating racer 2
 redBull = Turtle()
 redBull.color("Red")
-redBull.shape("circle")
+redBull.shape("square")
 
-Mercedes()
-redBull()
+#moving racers to start
+Mercedes.penup()
+Mercedes.goto(-300,0)
+redBull.penup()
+redBull.goto(-300,30)
+
+#drawing finishline using Racer1
+Mercedes.goto(300,-250)
+Mercedes.left(90)
+Mercedes.pendown()
+Mercedes.color("white")
+Mercedes.forward(500)
+
+#writing "Finish Line"
+Mercedes.pensize(10)
+Mercedes.color("white")
+Mercedes.write("Finish Line!", font =("Verdana", 10 , "bold"))
+
+Mercedes.penup()
+Mercedes.goto(-300,0)
+Mercedes.color("blue")
+
+
 
 
 

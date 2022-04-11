b8ff032955d2ec4cdc2f4ce35011e998e0db9620
Trevor <notmetro2008@gmail.com>
2022-04-11 18:56:02 +0100

    commiting screen and turtle objects

---
+++ main.py
@@ -0,0 +1,22 @@
+from turtle import *
+import turtle
+#creating screen
+screen = turtle.screen()
+screen.bgcolor("grey")
+
+
+#
+Mercedes = Turtle()
+Mercedes.color("Blue")
+Mercedes.shape("square")
+
+redBull = Turtle()
+redBull.color("Red")
+redBull.shape("circle")
+
+Mercedes()
+redBull()
+
+
+
+exitonclick()
